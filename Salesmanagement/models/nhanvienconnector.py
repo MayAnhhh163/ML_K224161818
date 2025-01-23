7518ebb39a6bad5aed16c6f1da9454472b474b81
MayAnhhh163 <huyenhuyen893@gmail.com>
2025-01-23 15:12:13 +0700

    23/01/25

---
+++ Salesmanagement/models/nhanvienconnector.py
@@ -0,0 +1,18 @@
+from Salesmanagement.libs.connector import MySQLConnector
+from Salesmanagement.models.nhanvien import NhanVien
+
+
+class NhanVienConnector(MySQLConnector):
+    def dang_nhap(self,username,password):
+        cursor=self.conn.cursor()
+        sql=('select * from nhanvien where username = %s and password=%s')
+        val=(username,password)
+        cursor.execute(sql, val)
+        dataset = cursor.fetchone()
+        nv=None #giả sử kh tìm thấy nhân viên đúng theo username và password
+        if dataset != None:
+            id, manhanvien, tennhanvien, username, password, isdeleted = dataset
+            #vào đc đây tức là có nvien
+            nv=NhanVien(id,manhanvien, tennhanvien, username, password, isdeleted)
+        cursor.close()
+        return nv #Null: đăng nhập thất bại
--- Salesmanagement/ui/LoginMainWindowExt.py
+++ Salesmanagement/ui/LoginMainWindowExt.py
@@ -1,10 +1,14 @@
-from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QMessageBox, QMainWindow
+
+from Salesmanagement.models.nhanvienconnector import NhanVienConnector
 from Salesmanagement.ui.LoginMainWindow import Ui_MainWindow
 from Salesmanagement.ui.MainProgramMainWindowExt import MainProgramMainWindowExt
 
 
 class LoginMainWindowExt(Ui_MainWindow):
+    def __init__(self):
+        self.nvconnector=NhanVienConnector()
+
     def setupUi(self, MainWindow):
         super().setupUi(MainWindow)
         self.MainWindow=MainWindow
@@ -19,7 +23,10 @@ class LoginMainWindowExt(Ui_MainWindow):
         username=self.lineEdit_username.text()
         password=self.lineEdit_password.text()
         #giả lập đăng nhập (hôm sau truy vấn thật trong CSDL)
-        if username=="admin" and password=="123":
+        #Goij kết nối đăng nhập trong MySQL
+        self.nvconnector.connect()
+        self.nvlogin=self.nvconnector.dang_nhap(username,password)
+        if self.nvlogin!=None:
             self.MainWindow.hide()
             self.mainwindow = QMainWindow()
             self.myui = MainProgramMainWindowExt()
